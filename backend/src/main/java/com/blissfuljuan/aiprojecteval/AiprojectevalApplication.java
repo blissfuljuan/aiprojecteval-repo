c2b06812201cf05a38ea53c861045f7e079545b6
blissfuljuan <cirevil.1989@gmail.com>
2026-04-25 22:25:37 +0800

    backend: Initial backend codebase

---
+++ backend/src/main/java/com/blissfuljuan/aiprojecteval/AiprojectevalApplication.java
@@ -0,0 +1,13 @@
+package com.blissfuljuan.aiprojecteval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiprojectevalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiprojectevalApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/blissfuljuan/aiprojecteval/AiprojectevalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blissfuljuan.aiprojecteval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiprojectevalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
